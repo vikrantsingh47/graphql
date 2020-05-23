75ed25561351058af5a7707c88867a22516cf63a
vikrantsingh47 <vikrantsingh47@gmail.com>
2020-05-23 18:11:15 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressGraphql = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+app.use('/graphql', expressGraphql({
+    schema: buildSchema(`
+        type Event{
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput{
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery{
+            events: [Event!]!
+        }
+
+        type RootMutation{
+            createEvent(eventInput: EventInput): Event
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return Event.find()
+            .then(events => {
+                return events.map( event => {
+                    return {...event._doc}
+                })
+            })
+            .catch(err => {
+                throw err;
+            });
+        },
+        createEvent: (args) => {
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date)
+            });
+            return event
+            .save().then(result => {
+                console.log(result);
+                return { ...result._doc };
+            }).catch(err => {
+                console.log(err);
+                throw err;
+            });
+        }
+    },
+    graphiql: true
+}));
+
+
+
+
+mongoose.connect(`mongodb+srv://@cluster0-xdaqi.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+                  { useNewUrlParser: true, 
+                    useUnifiedTopology: true,
+                    user: process.env.MONGO_USER, 
+                    pass: process.env.MONGO_PASSWORD, })
+                  .then(()=>{
+                      app.listen(3000);
+                  })
+                  .catch(err => {
+                      console.log(process.env.MONGO_USER);
+                      console.log(err);
+                  });
+
+
+
+/*
+query{
+  events{
+    title
+    description
+  }
+}
+
+mutation {
+  createEvent(eventInput: {title: "test", description: "this is a test", price: 99.99, date: "2020-04-14T07:56:46.549Z"}) {
+    title
+  }
+}
+
+
+new Date().toISOString();
+
+
+Here is the string for the DB-connection: 
+
+`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-cpkkj.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+
+Please note that you have to provide it without a RETURN/LINEFEED in between
+
+
+//if you get the error while requesting data regarding _id, 
+return { ...result._doc, _id: result._doc._id.toString()}, or,
+return { ...result._doc, result.id } // the result.id is different from _id. result.id is a getter 
+provided by mongoose.
+*/
